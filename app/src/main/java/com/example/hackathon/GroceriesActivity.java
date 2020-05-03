a3f5905dfadd63c37605f308aa0d059a71a60d59
aadhitya98 <aadhitya98@gmail.com>
2020-05-03 17:40:50 +0530

    Aadhitya made Changes

--- app/src/main/java/com/example/hackathon/GroceriesActivity.java
+++ app/src/main/java/com/example/hackathon/GroceriesActivity.java
@@ -1,14 +1,341 @@
 package com.example.hackathon;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-public class GroceriesActivity extends AppCompatActivity {
+import androidx.core.app.ActivityCompat;
+
+
+public class GroceriesActivity extends Activity {
+
+    // flag for Internet connection status
+    Boolean isInternetPresent = false;
+    int PERMISSION_ID = 44;
+
+    // Connection detector class
+    ConnectionDetector cd;
+
+    // Alert Dialog Manager
+    AlertDialogManager alert = new AlertDialogManager();
+
+    // Google Places
+    GooglePlaces googlePlaces;
+
+    // Places List
+    PlacesList nearPlaces;
+
+    // GPS Location
+    GPSTracker gps;
+
+    // Button
+    Button btnShowOnMap;
+
+    // Progress dialog
+    ProgressDialog pDialog;
+
+    // Places Listview
+    ListView lv;
+
+    // ListItems data
+    ArrayList<HashMap<String, String>> placesListItems = new ArrayList<HashMap<String,String>>();
+
+
+    // KEY Strings
+    public static String KEY_REFERENCE = "reference"; // id of the place
+    public static String KEY_NAME = "name"; // name of the place
+    public static String KEY_VICINITY = "vicinity"; // Place area name
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groceries);
+
+        cd = new ConnectionDetector(getApplicationContext());
+
+        // Check if Internet present
+        isInternetPresent = cd.isConnectingToInternet();
+
+        if (!isInternetPresent) {
+            // Internet Connection is not present
+            alert.showAlertDialog(GroceriesActivity.this, "Internet Connection Error",
+                    "Please connect to working Internet connection", false);
+            // stop executing code by return
+            return;
+        }
+
+        // creating GPS Class object
+        gps = new GPSTracker(this);
+
+        // check if GPS location can get
+        if (gps.canGetLocation()) {
+            Log.d("Your Location", "latitude:" + gps.getLatitude() + ", longitude: " + gps.getLongitude());
+        } else {
+            // Can't get user's current location
+            alert.showAlertDialog(GroceriesActivity.this, "GPS Status",
+                    "Couldn't get location information. Please enable GPS",
+                    false);
+            // stop executing code by return
+            return;
+        }
+
+        // Getting listview
+        lv = findViewById(R.id.list);
+
+        // button show on map
+        //btnShowOnMap = findViewById(R.id.btn_show_map);
+
+        // calling background Async task to load Google Places
+        // After getting places from Google all the data is shown in listview
+
+
+        new LoadPlaces().execute();
+
+        /** Button click event for shown on map */
+     /*  btnShowOnMap.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(getApplicationContext(),
+                       GroceriesActivity.class);
+                // Sending user current geo location
+                i.putExtra("user_latitude", Double.toString(gps.getLatitude()));
+                i.putExtra("user_longitude", Double.toString(gps.getLongitude()));
+
+                // passing near places to map activity
+                i.putExtra("near_places", nearPlaces);
+                // staring activity
+                startActivity(i);
+            }
+        });*/
+
+
+        /**
+         * ListItem click event
+         * On selecting a listitem SinglePlaceActivity is launched
+         * */
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+               String reference = ((TextView) view.findViewById(R.id.reference)).getText().toString();
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(),
+                       MainActivity.class);
+
+                // Sending place refrence id to single place activity
+                // place refrence id used to get "Place full details"
+                in.putExtra(KEY_REFERENCE, reference);
+                startActivity(in);
+            }
+        });
     }
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                new LoadPlaces().execute();
+            }
+        }
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        //new LoadPlaces().execute();
+        }
+
+
+
+    /**
+     * Background Async Task to Load Google places
+     * */
+    class LoadPlaces extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(GroceriesActivity.this);
+            pDialog.setMessage(Html.fromHtml("<b>Search</b><br/>Loading Places..."));
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting Places JSON
+         * */
+        protected String doInBackground(String... args) {
+
+            // creating Places class object
+            googlePlaces = new GooglePlaces();
+
+            try {
+                // Separeate your place types by PIPE symbol "|"
+                // If you want all types places make it as null
+                // Check list of types supported by google
+                //
+                String types = "supermarket|store"; // Listing places only cafes, restaurants
+
+                // Radius in meters - increase this value if you don't find any places
+                double radius = 2000; // 1000 meters
+
+                // get nearest places
+                nearPlaces = googlePlaces.search(gps.getLatitude(),
+                        gps.getLongitude(), radius, types);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * and show the data in UI
+         * Always use runOnUiThread(new Runnable()) to update UI from background
+         * thread, otherwise you will get error
+         * **/
+        protected void onPostExecute(String file_url) {
+            super.onPostExecute(file_url);
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed Places into LISTVIEW
+                     * */
+                    // Get json response status
+                    String status = nearPlaces.status;
+
+                    // Check for all possible status
+                    if(status.equals("OK")){
+                        // Successfully got places details
+                        if (nearPlaces.results != null) {
+                            // loop through each place
+                            for (Place p : nearPlaces.results) {
+                                HashMap<String, String> map = new HashMap<String, String>();
+                                    Log.i("Places",p.toString());
+                                // Place reference won't display in listview - it will be hidden
+                                // Place reference is used to get "place full details"
+                                map.put(KEY_REFERENCE, p.reference);
+
+                                // Place name
+                                map.put(KEY_NAME, p.name);
+
+
+                                // adding HashMap to ArrayList
+                                placesListItems.add(map);
+                            }
+                            // list adapter
+                            ListAdapter adapter = new SimpleAdapter(GroceriesActivity.this, placesListItems,
+                                    R.layout.list_item,
+                                    new String[] { KEY_REFERENCE, KEY_NAME}, new int[] {
+                                    R.id.reference, R.id.name });
+
+                            // Adding data into listview
+                            lv.setAdapter(adapter);
+                        }
+                    }
+                    else if(status.equals("ZERO_RESULTS")){
+                        // Zero results found
+                        alert.showAlertDialog(GroceriesActivity.this, "Near Places",
+                                "Sorry no places found. Try to change the types of places",
+                                false);
+                    }
+                    else if(status.equals("UNKNOWN_ERROR"))
+                    {
+                        alert.showAlertDialog(GroceriesActivity.this, "Places Error",
+                                "Sorry unknown error occured.",
+                                false);
+                    }
+                    else if(status.equals("OVER_QUERY_LIMIT"))
+                    {
+                        alert.showAlertDialog(GroceriesActivity.this, "Places Error",
+                                "Sorry query limit to google places is reached",
+                                false);
+                    }
+                    else if(status.equals("REQUEST_DENIED"))
+                    {
+                        alert.showAlertDialog(GroceriesActivity.this, "Places Error",
+                                "Sorry error occured. Request is denied",
+                                false);
+                    }
+                    else if(status.equals("INVALID_REQUEST"))
+                    {
+                        alert.showAlertDialog(GroceriesActivity.this, "Places Error",
+                                "Sorry error occured. Invalid Request",
+                                false);
+                    }
+                    else
+                    {
+                        alert.showAlertDialog(GroceriesActivity.this, "Places Error",
+                                "Sorry error occured.",
+                                false);
+                    }
+                }
+            });
+
+        }
+
+    }
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }*/
+
+
+
 }
