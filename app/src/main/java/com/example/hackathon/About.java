4659363e6f185901ba9809cdb828ab368e2afd8e
aadhitya98 <aadhitya98@gmail.com>
2020-05-14 22:43:44 +0530

    about us added

--- app/src/main/java/com/example/hackathon/About.java
+++ app/src/main/java/com/example/hackathon/About.java
@@ -2,9 +2,14 @@ package com.example.hackathon;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class About extends AppCompatActivity {
 
@@ -16,6 +21,18 @@ public class About extends AppCompatActivity {
         about.setTitle("About");
         about.setTitleTextColor(Color.WHITE);
         about.setBackgroundColor(Color.parseColor("#1F5BF3"));
+        AboutPage aboutPage = new AboutPage(getApplicationContext())
+                .isRTL(false)
+                .setImage(R.mipmap.fixanappointment)
+                .setDescription(getString(R.string.about_us_description))
+                .addItem(new Element("Version " + BuildConfig.VERSION_NAME, R.drawable.ic_info))
+                .addGroup("Connect with us")
+                .addFacebook(getString(R.string.FACEBOOK_ID))
+                .addEmail(getString(R.string.GMAIL_ID));
+        //LinearLayout parentLayout = (LinearLayout)findViewById(R.id.parent);
+        View aboutPageView = aboutPage.create();
+       // parentLayout.addView(aboutPageView);
+        setContentView(aboutPageView);
 
     }
 }
